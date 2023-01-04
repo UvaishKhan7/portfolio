b12c3ef0a2abffbf339028fdde5eeb2745294360
UvaishKhan7 <uvaish8248@gmail.com>
2023-01-04 20:44:58 +0530

    github & live links updated

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -14,56 +14,56 @@ const data = [
     image: IMG1,
     title: 'Fit-Clan',
     desc: '(ReactJS, Firebase, MaterialUI, React-Bootstrap etc.)',
-    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
-    demo: 'https://imdb-clone-mu.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/fitClan',
+    demo: 'https://fit-clan-lovat.vercel.app/'
   },
   {
     id: 2,
     image: IMG2,
     title: 'Slack Clone',
     desc: '(ReactJS, Firebase, React-Bootstrap)',
-    github: 'https://github.com/UvaishKhan7/portfolio',
-    demo: 'https://uvaish.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/slack-clone',
+    demo: 'https://slack-clone-uvaishkhan7.vercel.app/'
   },
   {
     id: 3,
     image: IMG3,
     title: 'IMDb Clone',
     desc: '(ReactJS, TMDb API, MaterialUI, React-Bootstrap etc.)',
-    github: 'https://github.com/UvaishKhan7/shopaholic',
-    demo: 'https://shopaholic-psi.vercel.app'
+    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
+    demo: 'https://imdb-clone-mu.vercel.app/'
   },
   {
     id: 4,
     image: IMG4,
     title: 'My Portfolio',
     desc: '(ReactJS, EmailJS, React-Icons)',
-    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
-    demo: 'https://imdb-clone-mu.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/portfolio',
+    demo: 'https://uvaish.vercel.app/'
   },
   {
     id: 5,
     image: IMG5,
     title: 'Shopaholic',
     desc: '(ReactJS, React-Bootstrap)',
-    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
-    demo: 'https://imdb-clone-mu.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/shopaholic',
+    demo: 'https://shopaholic-psi.vercel.app'
   },
   {
     id: 6,
     image: IMG6,
     title: 'Pair Game',
     desc: '(HTML, Javascript, CSS)',
-    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
-    demo: 'https://imdb-clone-mu.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/Pairs-Game',
+    demo: 'https://pairs-game-gamma.vercel.app/'
   },
   {
     id: 7,
     image: IMG7,
     title: 'Age Calculator',
     desc: 'HTML, Javascript, CSS',
-    github: 'https://github.com/UvaishKhan7/IMDb_Clone',
-    demo: 'https://imdb-clone-mu.vercel.app/'
+    github: 'https://github.com/UvaishKhan7/Age_Calculator',
+    demo: 'https://age-calculator-iota.vercel.app/'
   }
 ];
 
