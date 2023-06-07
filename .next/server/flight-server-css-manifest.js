a64792e10e5959c7b7373eebd10563160680eff4
UvaishKhan7 <uvaish8248@gmail.com>
2023-06-07 11:57:17 +0530

    portfolio on next - home & contact page created

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "E:\\Projects\\portfolio\\app\\layout.js": [
+      "E:\\Projects\\portfolio\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.js\",\"import\":\"Roboto\",\"arguments\":[{\"weight\":[\"100\",\"300\",\"400\",\"500\",\"700\",\"900\"],\"subsets\":[\"latin\"]}],\"variableName\":\"roboto\"}",
+      "E:\\Projects\\portfolio\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "E:\\Projects\\portfolio\\app\\page": [
+      "E:\\Projects\\portfolio\\app\\globals.css",
+      "E:\\Projects\\portfolio\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.js\",\"import\":\"Roboto\",\"arguments\":[{\"weight\":[\"100\",\"300\",\"400\",\"500\",\"700\",\"900\"],\"subsets\":[\"latin\"]}],\"variableName\":\"roboto\"}"
+    ],
+    "E:\\Projects\\portfolio\\app\\contact\\page": [
+      "E:\\Projects\\portfolio\\app\\globals.css",
+      "E:\\Projects\\portfolio\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.js\",\"import\":\"Roboto\",\"arguments\":[{\"weight\":[\"100\",\"300\",\"400\",\"500\",\"700\",\"900\"],\"subsets\":[\"latin\"]}],\"variableName\":\"roboto\"}"
+    ]
+  }
+}
